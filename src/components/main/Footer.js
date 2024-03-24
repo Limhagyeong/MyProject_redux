caf23e40ac79d51b6e913b465645db63365a6888
Limhagyeong <aamumu88@gmail.com>
2024-03-24 22:23:36 +0900

    리덕스 프로젝트(1)

--- src/components/main/Footer.js
+++ src/components/main/Footer.js
@@ -1,14 +1,55 @@
 export const Footer=()=>{
     return(
-    <>
-        <footer>
-            <div className="container">
-                <div className="row">
+        <>
+            <nav className="hidden-xs hidden-sm navbar footer-nav" role="navigation">
+                <div className="container">
 
+                    <div className="navbar-header">
+
+                        <div className="navbar-brand">
+                            <span className="sr-only">&copy;Wellness Companion</span>
+                            <a href="index.html">
+                                &copy;Wellness Companion
+                            </a>
+                        </div>
+
+
+                    </div>
+
+                    <div className="collapse navbar-collapse" id="main-nav-collapse">
+                        <ul className="nav navbar-nav navbar-right text-capitalize">
+                            <li><a href="#about">
+                                <span>home</span>
+                            </a></li>
+
+                            <li><a href="#services">
+                                <span>pages</span>
+                            </a></li>
+
+                            <li><a href="#portfolio">
+                                <span>features</span>
+                            </a></li>
+
+                            <li><a href="#team">
+                                <span>portfolio</span>
+                            </a></li>
+
+                            <li><a href="#pricing">
+                                <span>blog</span>
+                            </a></li>
+
+                            <li><a href="#blog">
+                                <span>shop</span>
+                            </a></li>
+
+                            <li><a href="#contact">
+                                <span>contact</span>
+                            </a></li>
+                        </ul>
+                    </div>
 
                 </div>
-            </div>
-        </footer>
-    </>
+            </nav>
+        </>
     )
 }
